57bd5639c8e8c191d1c6be69279d6e7e87d1f25d
Jahon214 <jahongir2140777@gmail.com>
2026-09-26 01:04:39 +0500

    changes3

--- app.js
+++ app.js
@@ -687,4 +687,29 @@
 //     console.log('code:' + event.code);
 // });
 
+ const getRandomNumbe = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+ }
 
+ const generate = () => {
+    const minEl = document.getElementById('min');
+    const maxEl = document.getElementById('max');
+    const min = Number(minEl.value);
+    const max = Number(maxEl.value);
+    console.log(min, max);
+
+    if(minEl.value === '' || maxEl.value === '') {
+        alert('Iltimos min va max qiymatlarini kiriting!');
+        return;
+    }
+
+    if(min > max) {
+        alert('Min maxdan kichik bolishi kerak!')
+    }
+
+    const placeholderEl = document.querySelector('#placeholder');
+    placeholderEl.textContent = getRandomNumbe(min, max);
+}
+
+ const btnEl = document.getElementById('generate');
+ btnEl.addEventListener('click', generate);
